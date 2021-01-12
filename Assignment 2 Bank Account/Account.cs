e9c5f96282682cb2725ca9cba999f4c3fd575a8e
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-12 14:58:44 +0700

    Add project files.

---
+++ Assignment 2 Bank Account/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2_Bank_Account
+{
+    class Account
+    {
+        private string name;
+        private double balance;
+
+        public Account(string nm, double bal)
+        {
+            name = nm;
+            balance = bal;
+        }
+
+        public double CheckBalance()
+        {
+            return balance;
+        }
+
+        public double WithdrawMoney(double MoneyOut)
+        {
+            if (MoneyOut > balance)
+            {
+                Console.WriteLine("You do not have enough money");
+            }
+            else if (MoneyOut > 0)
+            {
+                Console.WriteLine("Withdraw successful!");
+                balance = balance - MoneyOut;
+            }
+            else
+            {
+                Console.WriteLine("No Scam!");
+            }
+            return balance;
+        }
+        public double DepositMoney(double MoneyIn)
+        {
+            if (MoneyIn > 0)
+            {
+                Console.WriteLine("Money added");
+                balance = balance + MoneyIn;
+            }
+            else
+            {
+                Console.WriteLine("Deposit failed!");
+            }
+            return balance;
+        }
+        //Interest rate is in the format 1.05%
+        //This means 5%
+        public double AddInterest(double InterestRate) 
+        {
+            string date = DateTime.Now.ToShortDateString();
+            Console.WriteLine(date);
+            if(date == "01/01/2021")
+            {
+                balance = balance * InterestRate;
+            }
+            return balance;
+        }
+    }
+}
+
+
+
+          
---
+++ Assignment 2 Bank Account/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment_2_Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account Intouch = new Account("Inn", 10);
+            Console.WriteLine(Intouch.CheckBalance());
+            Console.WriteLine(Intouch.WithdrawMoney(3));
+            Console.WriteLine(Intouch.DepositMoney(100));
+            Console.WriteLine(Intouch.AddInterest(1.05));
+        }
+    }
+}
